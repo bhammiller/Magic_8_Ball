9caba220cb3ded470a5af74ba6c1d26e724b65e5
Brandon Hammiller <bdayton1985@gmail.com>
2018-01-25 16:46:14 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Magic8Ball.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+public class Magic8Ball {
+    public String magic8Ball(){
+        Integer magic_number = 0;
+        String magic_response;
+        switch(magic_number){
+            case 1 : magic_response = "It is certain";
+                break;
+            case 2 : magic_response = "It is decidedly so";
+                break;
+            case 3 : magic_response = "Without a doubt";
+                break;
+            case 4 : magic_response = "Yes definitely";
+                break;
+            case 5 : magic_response = "You may rely on it";
+                break;
+            case 6 : magic_response = "As I see it, yes";
+                break;
+            case 7 : magic_response = "Most likely";
+                break;
+            case 8 : magic_response = "Outlook good";
+                break;
+            case 9 : magic_response = "Yes";
+                break;
+            case 10 : magic_response = "Signs point to yes";
+                break;
+            case 11 : magic_response = "Reply hazy try again";
+                break;
+            case 12 : magic_response = "Ask again later";
+                break;
+            case 13 : magic_response = "Better not tell you now";
+                break;
+            case 14 : magic_response = "Cannot predict now";
+                break;
+            case 15 : magic_response = "Concentrate and ask again";
+                break;
+            case 16 : magic_response = "Don't count on it";
+                break;
+            case 17 : magic_response = "My reply is no";
+                break;
+            case 18 : magic_response = "My sources say no";
+                break;
+            case 19 : magic_response = "Outlook not so good";
+                break;
+            case 20 : magic_response = "Very doubtful";
+                break;
+        }
+    }
+}
